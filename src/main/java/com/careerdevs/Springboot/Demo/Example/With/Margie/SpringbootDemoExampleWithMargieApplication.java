aec5960e4279740c874d063d12ddfb0c6676eecc
darielhernandez <13dariel@gmail.com>
2022-05-03 17:24:07 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/Springboot/Demo/Example/With/Margie/SpringbootDemoExampleWithMargieApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.Springboot.Demo.Example.With.Margie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoExampleWithMargieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoExampleWithMargieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/Springboot/Demo/Example/With/Margie/SpringbootDemoExampleWithMargieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.Springboot.Demo.Example.With.Margie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoExampleWithMargieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
